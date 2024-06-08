d3aff005fd4278747b3fbd7c13bd7e99be1f98f6
Stiven1999A <eandrey.gonzalez@udea.edu.co>
2024-06-08 00:26:02 -0500

    Actualización

--- API.py
+++ API.py
@@ -1,7 +1,23 @@
-import requests as rq 
+"""Desarrollo del punto 3"""
 
-rq.post(url='https://my-json-server.typicode.com/Stiven1999A/Programacion_Para_La_Industria/posts',data={
+import requests
+url='https://my-json-server.typicode.com/Stiven1999A/Actividad_PI/db.json'
+response = requests.post(url, timeout=10)
+data = response.json()
+print(data)
+requests.post(
+    url='https://my-json-server.typicode.com/Stiven1999A/Programacion_Para_La_Industria/posts',
+    data={'title': 'foo',
+          'body': 'bar',
+          'userId': 1,
+          },
+    timeout=10)
+
+requests.put(
+    url='https://my-json-server.typicode.com/Stiven1999A/Programacion_Para_La_Industria/posts/1',
+    data={
     'title': 'foo',
     'body': 'bar',
     'userId': 1,
-  })
+    },
+    timeout=10)
